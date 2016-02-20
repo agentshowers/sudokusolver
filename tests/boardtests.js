c2e117be648e02b4c32f1bc467e3c7ab5bece52e
agentshowers <jmrcamorim@outlook.com>
2016-02-20 22:17:14 +0000

    Added toJSON tests

--- tests/boardtests.js
+++ tests/boardtests.js
@@ -6,4 +6,22 @@ QUnit.test( "Validate board from JSON", function( assert ) {
 	var someBoard = createSomeBoard();
 	fromJSON = boardFromJSON(jsonSomeBoard);
 	assert.boardEquals(fromJSON, someBoard, "Check if random board is correctly read from JSON");
+	
+	var fullBoard = createFullBoard();
+	fromJSON = boardFromJSON(jsonFullBoard);
+	assert.boardEquals(fromJSON, fullBoard, "Check if full board is correctly read from JSON");
+});
+
+QUnit.test( "Validate board to JSON", function( assert ) {
+	var aBoard = new board();
+	var json = aBoard.toJSON();
+	assert.equal(json, jsonEmptyBoard, "Check if empty board is correctly written to JSON");
+	
+	aBoard = createSomeBoard();
+	json = aBoard.toJSON();
+	assert.equal(json, jsonSomeBoard, "Check if random board is correctly written to JSON");
+	
+	aBoard = createFullBoard();
+	json = aBoard.toJSON();
+	assert.equal(json, jsonFullBoard, "Check if full board is correctly written to JSON");
 });
